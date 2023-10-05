6ff1788bf22ff4d7f72a231e13347fccfc79f363
Santiago Cuesta <santiago.cuesta.f@gmail.com>
2023-10-04 22:31:23 -0300

    preentrega3

--- js/script.js
+++ js/script.js
@@ -42,13 +42,14 @@ class producto{
 //array de productos disponibles en tienda
 let galeriaProductos = [
     new producto('Curso Específico', 1, 10, 'curso'),
-    //new producto('cursoOnline2', 1, 10, 'curso'),
-    //new producto('cursoOnline3', 1, 10, 'curso'),
+    new producto('Curso Específico 2', 1, 10, 'curso'),
+    new producto('Curso Específico 3', 1, 10, 'curso'),
     new producto('Suscripción Cursos Online', 5, 10, 'curso'),
     new producto('Taller Principiante', 420, 10, 'taller'),
     new producto('Taller Intermedio', 640, 10, 'taller'),
     new producto('Taller Experto', 800, 10, 'taller'),
-    new producto('Consultoria de Proyectos', 120, 10, 'consultoria')
+    new producto('Consultoria de Proyectos', 120, 10, 'consultoria'),
+    new producto('Consultoria general TI', 120, 10, 'consultoria')
 ]
 
 //inicializamos el carrito de compra copmo array
@@ -56,25 +57,142 @@ let carritoProductos = []
 
 //fncion que permite obtener el nombre del cliente
 function solicitaNombre(){
-    while(nombreCliente == ''){
-        nombreCliente = prompt('Bienvenido! ingresa tu nombre completo')
-    }
+    let menuPrincipal = document.getElementById('ingresa-nombre')
+
+    let mensajeBienvenida = document.createElement('h3')
+    mensajeBienvenida.innerText = 'Bienvenido! ingresa tu nombre completo'
+    menuPrincipal.append(mensajeBienvenida)
+
+    let formNombre = document.createElement('form')
+    formNombre.setAttribute("id", "formNombre")
+
+    let inputName = document.createElement('input')
+    inputName.setAttribute("type", "text")
+    inputName.setAttribute("id", "inputName")
+    formNombre.append(inputName)
+
+    let submitInputName = document.createElement('input')
+    submitInputName.setAttribute("type", "submit")
+    submitInputName.setAttribute("value", "Ingresar")
+    formNombre.append(submitInputName)
+    
+    menuPrincipal.append(formNombre)
+
+    //evento para form submit
+    //formNombre.addEventListener("submit", ingresaNombreSubmit)
+    formNombre.onsubmit = (e) => {
+        nombreCliente = document.getElementById('inputName').value
+        console.log(nombreCliente)
+
+        //revisamos si ya tenia carrito de compras!
+        let clienteEnLS = JSON.parse(localStorage.getItem(nombreCliente) )
+        if (clienteEnLS){
+            //transformamos la data e objetos y precargamos el carrito de compras
+            menuPrincipal.innerHTML = ''
+            for (let obj of clienteEnLS){
+                carritoProductos.push(new producto(obj.nombre, obj.precio, obj.stock, obj.tipo))
+            }
+            let msj = document.createElement('h3')
+            msj.innerText = 'Que gusto tenerte de vuelta '+nombreCliente+' :) !'
+            menuPrincipal.append(msj)
+            msj = document.createElement('p')
+            msj.innerText = 'Gracias por volver a preferirnos! te recuerdo que tienes '+carritoProductos.length+' productos e el carrito'
+            menuPrincipal.append(msj)
+        }
+        menuInicial(nombreCliente)
+        e.preventDefault()
+      }
+
 }
 
 //Menu principal con acciones
-function menuInicial(){
-    codServicio = parseInt(prompt('Como deseas que te ayudemos '+nombreCliente+' ?' +'\n'
-                                    +'Igresa una de las siguientes opciones:'+'\n'
-                                    +'(1 - Cursos Online)'+'\n'
-                                    +'(2 - Talleres)'+'\n'
-                                    +'(3 - Proyectos/Consultoria)'+'\n'
-                                    +'(4 - Ver Carrito de Compras)'+'\n'
-                                    +'(5 - Salir)'))
+function menuInicial(nombreCliente){
+    let menuPrincipal = document.getElementById('menu-principal')
+
+    let bienvenida = document.createElement('h3')
+    bienvenida.innerText = 'Como deseas que te ayudemos '+nombreCliente+' ?'
+
+    let listaMenu = document.createElement('ul')
+    opcionesMenu = ['(1 - Cursos Online)',
+                    '(2 - Talleres)', 
+                    '(3 - Proyectos/Consultoria)', 
+                    '(4 - Ver Carrito de Compras)',
+                    '(5 - Salir)']
+    for (let opcion of opcionesMenu){
+        let li = document.createElement('li')
+        //li.innerText = opcion
+        let btn = document.createElement('button');
+        btn.setAttribute("id", opcion);
+        btn.innerText = opcion
+        btn.onclick  = (e) => {
+            let botonSeleccionado = btn.innerText
+            console.log(botonSeleccionado)
+            subMenu(botonSeleccionado)
+            e.preventDefault()
+        }
+        li.append(btn)
+        listaMenu.append(li)
+    }
+
+    //append de elementos dle menu priccipal
+    menuPrincipal.append(bienvenida)
+    menuPrincipal.append(listaMenu)
 }
 
-//DEPRECAR - fncion que totaliza valores
-function actualizaTotalCompra(monto){
-    totalCompra = totalCompra + monto
+//funcion para saber que submenu mostrar en detalle-menu
+function subMenu(botonSeleccionado){
+    let detalleMenu = document.getElementById('detalle-menu')
+    detalleMenu.innerHTML = ''
+    switch(botonSeleccionado){
+        case '(1 - Cursos Online)':
+            console.log('mostrar opciones de cursos online')
+            menuCursosOnline()
+            break
+        case '(2 - Talleres)':
+            console.log('mostrar opciones de talleres')
+            menuTalleres()
+            break
+        case '(3 - Proyectos/Consultoria)':
+            console.log('mostrar opciones de proyecto')
+            menuConsultoria()
+            break
+        case '(4 - Ver Carrito de Compras)':
+            console.log('mostrar carrito de compra')
+            mostrarCarrito()
+            break
+        case '(5 - Salir)':
+            console.log('cerrar sesion')
+            guardarLocal()
+            break
+        default:
+            console.log('default')
+            break
+    }
+}
+
+function guardarLocal(nombre, carrito){
+    localStorage.setItem(nombreCliente, JSON.stringify(carritoProductos))
+    //reiniciamos web
+    let div = document.getElementById('ingresa-nombre')
+    div.innerHTML=''
+    div = document.getElementById('menu-principal')
+    div.innerHTML=''
+    div = document.getElementById('detalle-menu')
+    div.innerHTML=''
+    carritoProductos = []
+    //comenzamos de nuevo
+    solicitaNombre()
+}
+
+//fncion que totaliza valores
+function actualizaTotalCompra(){
+    let totalPrecio = 0
+    carritoProductos.forEach( (prod) => {
+        totalPrecio = totalPrecio + prod.precio
+    } )
+
+    let msjEvento = document.getElementById('msjEvento')
+    msjEvento.innerText = 'Valor Total del Carrito: $'+totalPrecio
 }
 
 /* 
@@ -87,15 +205,43 @@ function agregarProdACarito(nombreProd){
     let objProd = galeriaProductos.find( (prod) => prod.nombre === nombreProd) 
     //agregamos a carrito
     carritoProductos.push(objProd)
+    console.log('producto añadido: '+objProd.nombre)
+    console.log('total productos'+carritoProductos)
 }
 
 
-function mostrarCarrito(carrito){
+function mostrarCarrito(){
+    let detalleMenu = document.getElementById('detalle-menu');
+
+    let mensajeDetalleMenu = document.createElement('h3');
+    mensajeDetalleMenu.innerText = 'detalle del carrito de compras:';
+    detalleMenu.append(mensajeDetalleMenu);
+
+    let listaMenu = document.createElement('ul');
+    opcionesMenu = galeriaProductos.filter(function (el) {
+        return el.tipo === 'curso'
+        });
+
+    carritoProductos.forEach( (prod) => {
+        console.log(prod.nombre);
+        let li = document.createElement('li');
+        li.innerText = 'Producto: '+prod.nombre+', Precio: '+prod.precio;
+        listaMenu.append(li);
+         } )
+    
+    detalleMenu.append(listaMenu);
+    let msjEvento = document.createElement('p');
+    msjEvento.setAttribute("id", 'msjEvento');
+    detalleMenu.append(msjEvento);
+    actualizaTotalCompra()
+
+    /*
     let listaProductos = ''
     carrito.forEach( (prod) => {
         listaProductos = listaProductos+'Producto: '+prod.nombre+' , Precio: '+prod.precio+'\n'
     } )
-    alert(listaProductos)
+    //alert(listaProductos)
+    */
 }
 
 function yaExisteEnCarrito(nombreProd){
@@ -115,131 +261,145 @@ METODOS DEL MENU
 */
 //submenu de Cursos Online y respectivo workflow
 function menuCursosOnline(){
-    switch(subCodServicio){
-        case 1:
-            actualizaTotalCompra(1)
-            agregarProdACarito(nombreProd="Curso Específico" )
-            alert('se ha añadido el servicio al carrito')
-            break
-        case 2:
-            actualizaTotalCompra(5)
-            agregarProdACarito(nombreProd="Suscripción Cursos Online" )
-            alert('se ha añadido el servicio al carrito')
-            break
-        case 3:
-            boolServicios = false
-            break
-        default:
-            console.log('default menuCursosOnline')
-            break
+    let detalleMenu = document.getElementById('detalle-menu');
+
+    let mensajeDetalleMenu = document.createElement('h3');
+    mensajeDetalleMenu.innerText = 'que clase de cursos online te interesa?';
+    detalleMenu.append(mensajeDetalleMenu);
+
+    mensajeDetalleMenu = document.createElement('p');
+    mensajeDetalleMenu.innerText = 'selecciona un elemento para agregar al carrito';
+    detalleMenu.append(mensajeDetalleMenu);
+
+    let listaMenu = document.createElement('ul');
+    opcionesMenu = galeriaProductos.filter(function (el) {
+        return el.tipo === 'curso'
+        });
+
+    for (let opcion of opcionesMenu){
+        console.log(opcion.nombre);
+        let li = document.createElement('li');
+        //li.innerText = opcion
+        let btn = document.createElement('button');
+        btn.setAttribute("id", opcion.nombre);
+        btn.innerText = opcion.nombre;
+        btn.onclick  = (e) => {
+            let botonSeleccionado = btn.innerText
+            console.log(botonSeleccionado)
+            let msjEvento = document.getElementById('msjEvento')
+            if ( yaExisteEnCarrito(botonSeleccionado) ){
+                msjEvento.innerText = 'producto '+botonSeleccionado+' ya existe en carrito!!'
+            }
+            else {
+                agregarProdACarito(botonSeleccionado)
+                msjEvento.innerText = 'producto '+botonSeleccionado+' agregado :) !!'
+            }
+            e.preventDefault()
+        };
+        li.append(btn);
+        listaMenu.append(li);
     }
+    detalleMenu.append(listaMenu);
+    let msjEvento = document.createElement('p');
+    msjEvento.setAttribute("id", 'msjEvento');
+    detalleMenu.append(msjEvento);
+
 }
 
 function menuTalleres(){
-    switch(subCodServicio){
-        case 1:
-            actualizaTotalCompra(420)
-            agregarProdACarito(nombreProd="Taller Principiante" )
-            alert('se ha añadido el servicio "Taller Principiante" al carrito')
-            break
-        case 2:
-            actualizaTotalCompra(640)
-            agregarProdACarito(nombreProd="Taller Intermedio" )
-            alert('se ha añadido el servicio "Taller Intermedio" al carrito')
-            break
-        case 3:
-            actualizaTotalCompra(800)
-            agregarProdACarito(nombreProd="Taller Experto" )
-            alert('se ha añadido el servicio "Taller Experto" al carrito')
-            break
-        case 4: 
-            boolServicios = false
-            break
-        default:
-            console.log('default menuTalleres')
-            break
+    let detalleMenu = document.getElementById('detalle-menu');
+
+    let mensajeDetalleMenu = document.createElement('h3');
+    mensajeDetalleMenu.innerText = 'que clase de taller te interesa?';
+    detalleMenu.append(mensajeDetalleMenu);
+
+    mensajeDetalleMenu = document.createElement('p');
+    mensajeDetalleMenu.innerText = 'selecciona un elemento para agregar al carrito';
+    detalleMenu.append(mensajeDetalleMenu);
+
+    let listaMenu = document.createElement('ul');
+    opcionesMenu = galeriaProductos.filter(function (el) {
+        return el.tipo === 'taller'
+        });
+
+    for (let opcion of opcionesMenu){
+        console.log(opcion.nombre);
+        let li = document.createElement('li');
+        //li.innerText = opcion
+        let btn = document.createElement('button');
+        btn.setAttribute("id", opcion.nombre);
+        btn.innerText = opcion.nombre;
+        btn.onclick  = (e) => {
+            let botonSeleccionado = btn.innerText
+            console.log(botonSeleccionado)
+            let msjEvento = document.getElementById('msjEvento')
+            if ( yaExisteEnCarrito(botonSeleccionado) ){
+                msjEvento.innerText = 'producto '+botonSeleccionado+' ya existe en carrito!!'
+            }
+            else {
+                agregarProdACarito(botonSeleccionado)
+                msjEvento.innerText = 'producto '+botonSeleccionado+' agregado :) !!'
+            }
+            e.preventDefault()
+        };
+        li.append(btn);
+        listaMenu.append(li);
     }
+    detalleMenu.append(listaMenu);
+    let msjEvento = document.createElement('p');
+    msjEvento.setAttribute("id", 'msjEvento');
+    detalleMenu.append(msjEvento);
 }
 
 function menuConsultoria(){
-    switch(subCodServicio){
-        case 1:
-            actualizaTotalCompra(120)
-            agregarProdACarito(nombreProd="Consultoria de Proyectos" )
-            alert('se ha añadido el servicio "Consultoria de proyectos" al carrito')
-            break
-        case 2:
-            boolServicios = false
-            break
-        default:
-            console.log('default menuConsultoria')
-            break
+
+    let detalleMenu = document.getElementById('detalle-menu');
+
+    let mensajeDetalleMenu = document.createElement('h3');
+    mensajeDetalleMenu.innerText = 'Nuestra oferta de Proyectos y consultorias';
+    detalleMenu.append(mensajeDetalleMenu);
+
+    mensajeDetalleMenu = document.createElement('p');
+    mensajeDetalleMenu.innerText = 'selecciona un elemento para agregar al carrito';
+    detalleMenu.append(mensajeDetalleMenu);
+
+    let listaMenu = document.createElement('ul');
+    opcionesMenu = galeriaProductos.filter(function (el) {
+        return el.tipo === 'consultoria'
+        });
+
+    for (let opcion of opcionesMenu){
+        console.log(opcion.nombre);
+        let li = document.createElement('li');
+        //li.innerText = opcion
+        let btn = document.createElement('button');
+        btn.setAttribute("id", opcion.nombre);
+        btn.innerText = opcion.nombre;
+        btn.onclick  = (e) => {
+            let botonSeleccionado = btn.innerText
+            console.log(botonSeleccionado)
+            let msjEvento = document.getElementById('msjEvento')
+            if ( yaExisteEnCarrito(botonSeleccionado) ){
+                msjEvento.innerText = 'producto '+botonSeleccionado+' ya existe en carrito!!'
+            }
+            else {
+                agregarProdACarito(botonSeleccionado)
+                msjEvento.innerText = 'producto '+botonSeleccionado+' agregado :) !!'
+            }
+            e.preventDefault()
+        };
+        li.append(btn);
+        listaMenu.append(li);
     }
+    detalleMenu.append(listaMenu);
+    let msjEvento = document.createElement('p');
+    msjEvento.setAttribute("id", 'msjEvento');
+    detalleMenu.append(msjEvento);
 }
 
 
 function main(){
     solicitaNombre()
-
-    while(boolMenuInicial){
-        boolServicios = true
-        menuInicial()
-        console.log('codigo sevricio '+codServicio)
-        if (codServicio >= 1 && codServicio <= 5){
-            // esta todo perfecto, seguimos
-            console.log('pase verificacion!!')
-            while(boolServicios){
-                console.log('dento de while!! boolServicios')
-                switch(codServicio){
-                    case 1:
-                        subCodServicio = parseInt(prompt('que clase de cursos online te interesa?'+'\n'
-                                                        +'(1 - CursoEspecifico 1USD)' + yaExisteEnCarrito('Curso Específico')+'\n'
-                                                        +'(2 - Subscripcion Anual 5USD)' + yaExisteEnCarrito('Suscripción Cursos Online')+'\n'
-                                                        +'(3 - Volver)'))
-                        menuCursosOnline()
-                        boolServicios = false
-                        break
-                    case 2:
-                        subCodServicio = parseInt(prompt('que clase de Taller te interesa?'+'\n'
-                                                        +'(1 - Principiante 420USD)' + yaExisteEnCarrito('Taller Principiante')+'\n'
-                                                        +'(2 - Intermedio 640USD)' + yaExisteEnCarrito('Taller Intermedio')+'\n'
-                                                        +'(3 - Experto 800USD)' + yaExisteEnCarrito('Taller Experto')+'\n'
-                                                        +'(4 - Volver'))
-                        menuTalleres()
-                        boolServicios = false
-                        break
-                    case 3:
-                        subCodServicio = parseInt(prompt('Los proyectos y consultorias se cobran por hora 120USD a hora, te interesa?' +'\n'
-                                                        +'(1 - SI 120USD)' + yaExisteEnCarrito('Consultoria de Proyectos')+'\n'
-                                                        +'(2 - Volver)'))
-                        
-                        menuConsultoria()
-                        boolServicios = false
-                        break
-                    case 4:
-                        mostrarCarrito(carritoProductos)
-                        //alert('el costo total de los servicios es: '+totalCompra+'USD')
-                        boolServicios = false
-                        break
-                    case 5:
-                        alert('Gracias por tu visita!')
-                        boolServicios = false
-                        boolMenuInicial = false
-                        break
-                    default:
-                        console.log('default while boolservicios')
-                        boolServicios = false
-                        break
-
-                        
-                }
-            }
-        }else{
-            alert('Asegurate que el codigo de la opcion se encuentre entre 1 y 5') 
-        }
-    }
 }
 
-
 main()
-
